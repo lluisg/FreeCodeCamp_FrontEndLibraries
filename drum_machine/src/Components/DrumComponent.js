7023c20e5e884e331a94d576c3df44bfe6d9b4a7
lluisg <lluis.guardia.f@gmail.com>
2023-01-31 13:29:29 +0100

    drum machine css
    updated great part of the css and styling of the drum machine project, adding functionaility of the on/off buttons

--- drum_machine/src/Components/DrumComponent.js
+++ drum_machine/src/Components/DrumComponent.js
@@ -1,6 +1,7 @@
 import React, { createElement } from 'react';
 import PropTypes from "prop-types";
 // import icons fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons"
 
 import sounds from "./sounds"
@@ -9,8 +10,8 @@ import sounds from "./sounds"
 
 const ButtonsDrum = (props) => {
   return (
-    <button id={props.sound} onClick={()=>props.handleClick(props.sound, props.letter)} className="drum-pad btn">
-      {props.letter}
+    <button id={props.sound} onClick={()=>props.handleClick(props.sound, props.letter)} className="drum-pad p-4 rounded border-0">
+      <b>{props.letter}</b>
       <audio id={props.letter} src={props.src} className="clip"></audio>
     </button>
   );
@@ -21,10 +22,32 @@ const ButtonsDrum = (props) => {
 const Options = (props) => {
   return (
     <div className="options-container p-0 m-0">
-      <a id="display">{props.value}</a>
+      <a className="logo">FCC<FontAwesomeIcon icon={faFreeCodeCamp} className="" /></a>
+      <ButtonOption name="Power" onClick={props.changePower} />
+      <p id="display" className='my-3'>{props.value}</p>
+      <Slider onChange={props.changeVolume} />
+      <ButtonOption name="Bank" onClick={props.changeBank} />
     </div>
     );
 };
+
+function Slider(props) {
+  return(
+    <input type="range" min="1" max="100" value="50" onChange={()=>props.onChange(this.value)} />
+  );
+}
+
+function ButtonOption(props) {
+  return(
+    <div id={props.name} className='btn-onoff mt-3'>
+      <p className='p-0 m-0'><b>{props.name}</b></p>
+      <div className="container-onoff p-1">
+        <div id={props.name=='Power'?'power-btn-off':'bank-btn-off'} className="btn-off hide-btn" onClick={props.onClick}></div>
+        <div id={props.name=='Power'?'power-btn-on':'bank-btn-on'} className="btn-on" onClick={props.onClick}></div>
+      </div>
+    </div>
+  );
+}
 
 
 // ---------------------------------- CENTRAL COMPONENT ---------------------------------------
@@ -33,11 +56,18 @@ class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current_sound: ''
+      start_text: '... ',
+      current_text: '... ',
+      current_bank: 'Smooth Piano Kit',
+      power: true,
+      volume: 50
     }
 
     this.handleClick = this.handleClick.bind(this);
     this.handleKey = this.handleKey.bind(this);
+    this.changeBank = this.changeBank.bind(this);
+    this.changePower = this.changePower.bind(this);
+    this.changeVolume = this.changeVolume.bind(this);
   }
 
   handleClick(song, key) {
@@ -53,7 +83,7 @@ class DrumMachine extends React.Component {
     
 
     this.setState({
-      current_sound: song
+      current_text: song
     })
   };
 
@@ -73,22 +103,67 @@ class DrumMachine extends React.Component {
   componentWillUnmount(){
     document.removeEventListener("keydown", this.handleKey);
   }
-  
+
+  changeButtonPosition(id_on, id_off, condition){
+    let btnon = document.getElementById(id_on)
+    let btnoff = document.getElementById(id_off)
+
+    if(condition){
+      // console.log('on->off')
+      btnon.classList.add("hide-btn");
+      btnoff.classList.remove("hide-btn");
+    }else{
+      // console.log('off->on')
+      btnon.classList.remove("hide-btn");
+      btnoff.classList.add("hide-btn");
+    }
+
+  }
+
+  changeBank(){
+    this.changeButtonPosition('bank-btn-on', 'bank-btn-off', this.state.current_bank=='Smooth Piano Kit')
+
+    this.state.current_bank === 'Heater Kit'
+      ? this.setState({current_bank: 'Smooth Piano Kit', current_text: 'Smooth Piano Kit'})
+      : this.setState({current_bank: 'Heater Kit', current_text: 'Heater Kit'})
+    console.log('change bank')
+  }
+
+  changePower(){
+    this.changeButtonPosition('power-btn-on', 'power-btn-off', this.state.power)
+
+    this.setState({
+      power: !this.state.power,
+      current_text: this.state.start_text
+    });
+    console.log('change power')
+  }
+
+  changeVolume(volume){
+    this.setState({
+      volume: volume,
+      current_text: 'Volume: '+volume
+    })
+  }
+
   render() {
-    const drumpads = sounds.map(element => {  return  <ButtonsDrum  key={element.id}
-                                                                    sound={element.sound}
-                                                                    letter={element.key}
-                                                                    src={element.src}
-                                                                    handleClick={this.handleClick}
+    const drumpads = sounds[this.state.current_bank].map(element => {  return  <ButtonsDrum key={element.id}
+                                                                                            sound={element.sound}
+                                                                                            letter={element.key}
+                                                                                            src={element.src}
+                                                                                            handleClick={this.handleClick}
                                                       />
                                               });
     return (
       <div className="base-container">
         <div id="drum-machine">
-          <div className="grid-btns">
+          <div className="grid-btns p-0 m-4">
             {drumpads}
           </div>
-          <Options value={this.state.current_sound} />
+          BUTTONS QUAN CLICKES
+          SLIDER
+          CANVIAR LOGO I TITOL DE LA WEB DE TOTS ELS PROJECTES FINS ARA, ESTA EL PREDETERMINAT DE REACT
+          <Options value={this.state.current_text} changePower={this.changePower} changeBank={this.changeBank} changeVolume={this.changeVolume} />
         </div>
       </div>
     );
--- drum_machine/src/Components/sounds.js
+++ drum_machine/src/Components/sounds.js
@@ -1,58 +1,116 @@
-const sounds = [
-  {
-    key: 'Q',
-    sound: 'Heater 1',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
-    id: 'heater1'
-  },
-  {
-    key: 'W',
-    sound: 'Heater 2',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
-    id: 'heater2'
-  },
-  {
-    key: 'E',
-    sound: 'Heater 3',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
-    id: 'heater3'
-  },
-  {
-    key: 'A',
-    sound: 'Heater 4',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
-    id: 'heater4'
-  },
-  {
-    key: 'S',
-    sound: 'Clap',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
-    id: 'clap'
-  },
-  {
-    key: 'D',
-    sound: 'Open-HH',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
-    id: 'openhh'
-  },
-  {
-    key: 'Z',
-    sound: 'Kick-n-Hat',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
-    id: 'kicknhat'
-  },
-  {
-    key: 'X',
-    sound: 'Kick',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
-    id: 'kick'
-  },
-  {
-    key: 'C',
-    sound: 'Closed-HH',
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
-    id: 'closedhh'
-  }
-]
+const sounds = {
+  'Heater Kit': [
+    {
+      key: 'Q',
+      sound: 'Heater 1',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      id: 'heater1'
+    },
+    {
+      key: 'W',
+      sound: 'Heater 2',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      id: 'heater2'
+    },
+    {
+      key: 'E',
+      sound: 'Heater 3',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      id: 'heater3'
+    },
+    {
+      key: 'A',
+      sound: 'Heater 4',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      id: 'heater4'
+    },
+    {
+      key: 'S',
+      sound: 'Clap',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      id: 'clap'
+    },
+    {
+      key: 'D',
+      sound: 'Open-HH',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      id: 'openhh'
+    },
+    {
+      key: 'Z',
+      sound: 'Kick-n-Hat',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      id: 'kicknhat'
+    },
+    {
+      key: 'X',
+      sound: 'Kick',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      id: 'kick'
+    },
+    {
+      key: 'C',
+      sound: 'Closed-HH',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      id: 'closedhh'
+    }
+  ],
+  'Smooth Piano Kit': [
+    {
+      key: 'Q',
+      sound: 'Chord 1',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+      id: 'chord1'
+    },
+    {
+      key: 'W',
+      sound: 'Chord 2',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+      id: 'chord2'
+    },
+    {
+      key: 'E',
+      sound: 'Chord 3',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+      id: 'chord3'
+    },
+    {
+      key: 'A',
+      sound: 'Shaker',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+      id: 'shaker'
+    },
+    {
+      key: 'S',
+      sound: 'Open HH',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+      id: 'openhh'
+    },
+    {
+      key: 'D',
+      sound: 'Closed HH',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+      id: 'closedhh'
+    },
+    {
+      key: 'Z',
+      sound: 'Punchy Kick',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+      id: 'punchykick'
+    },
+    {
+      key: 'X',
+      sound: 'Side Stick',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+      id: 'sidestick'
+    },
+    {
+      key: 'C',
+      sound: 'Snare',
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+      id: 'snare'
+    }
+  ]
+}
 
 export default sounds;
